0330b965c4ae10796572525ad1e4e876e978af3f
Michael Pearson <mipearson@gmail.com>
2014-07-16 10:04:26 +1000

    Bring in excel_csv code.

--- lib/excel_csv.rb
+++ lib/excel_csv.rb
@@ -1,5 +1,70 @@
-require "excel_csv/version"
+require 'csv'
 
-module ExcelCsv
-  # Your code goes here...
+# Read & Write Microsoft Excel compliant CSV files, including basic
+# support for Excel running on Windows in Germany.
+class ExcelCSV
+
+  class << self
+    def encoding
+      Encoding::Windows_1252
+    end
+
+    def read filename, options = {}, &block
+      file = File.open filename, external_encoding: encoding, internal_encoding: 'UTF-8'
+      _read file, options, &block
+    end
+
+    def parse string, options = {}, &block
+      file = StringIO.new(string)
+      _read file, options, &block
+    end
+
+    def generate options = {}, &block
+      raise ArgumentError, "Block required" unless block_given?
+      s = CSV.generate({ row_sep: "\r\n" }.merge(options), &block)
+      encode_for_excel("sep=,\r\n" + s)
+    end
+
+    def write filename, options = {}, &block
+      raise ArgumentError, "Block required" unless block_given?
+      File.open(filename, 'wb') do |f|
+        f.write generate(options, &block)
+      end
+    end
+
+    private
+
+    def _read file, options, &block
+      col_sep = discover_col_sep file
+      CSV.parse(file, { col_sep: col_sep }.merge(options), &block)
+    end
+
+    # Microsoft Excel, on Mac and Windows, expects to import ANSI (Windows-1252)
+    # rather than UTF-8. Importing UTF-8 is possible, but only in Excel 2013, and
+    # has problems with line endings.
+    def encode_for_excel s
+      s.encode(encoding, invalid: :replace, undef: :replace)
+    end
+
+    def discover_col_sep file
+      first_line = file.readline
+      # Search for sep=x first line & return separator
+      found_sep = /^sep=(.)$/.match(first_line.strip)
+      return found_sep[1] if found_sep
+
+      file.rewind # Rewind, as we want CSV to be able to read the header
+
+      # Count commas & semicolons, pick whichever occurs most in the
+      # first line. This isn't 100% accurate, but as we can't rely
+      # on Germany's Excel to emit the correct sep=; line, we need
+      # to do something.
+      #
+      # Alternative is to rely on localisation, but I want spreadsheets
+      # to be portable between verticals.
+
+      first_line.count(';') > first_line.count(',') ? ';' : ','
+    rescue EOFError
+      ','
+    end
+  end
 end
---
+++ spec/excel_csv_spec.rb
@@ -0,0 +1,133 @@
+require 'rspec'
+require 'excel_csv'
+require 'tempfile'
+require 'active_support'
+require 'active_support/core_ext/string'
+
+# rubocop:disable UnneededPercentQ
+# https://github.com/bbatsov/rubocop/issues/1210
+
+describe 'ExcelCSV' do
+  def write_csv s, options = {}
+    @spreadsheet_file = Tempfile.new(%w{spreadsheet .csv}, options)
+    @spreadsheet_file.write s
+    @spreadsheet_file.close
+    @spreadsheet_file.path
+  end
+
+  after do
+    @spreadsheet_file.delete if @spreadsheet_file
+  end
+
+  describe 'read' do
+    let(:expected) { [["Ad ID", "Custom Code"]] }
+
+    it "should read in a regular CSV" do
+      rows = ExcelCSV.read write_csv("Ad ID,Custom Code\n")
+      expect(rows).to eq(expected)
+    end
+
+    it "should read in a spreadsheet with a sep=; special first line" do
+      rows = ExcelCSV.read write_csv <<-EOF.strip_heredoc
+        sep=;
+        Ad ID;Custom Code
+      EOF
+
+      expect(rows).to eq(expected)
+    end
+
+    it "should auto-detect semicolons as the separator, even if missing the sep= line" do
+      rows = ExcelCSV.read write_csv("Ad ID;Custom Code\n")
+      expect(rows).to eq(expected)
+    end
+
+    it "should read in a DOS-encoded spreadsheet with a sep=; special first line" do
+      rows = ExcelCSV.read write_csv <<-EOF.strip_heredoc.gsub("\n", "\r\n")
+        sep=;
+        Ad ID;Custom Code
+      EOF
+
+      expect(rows).to eq(expected)
+    end
+
+    it "should read in Windows-1252 encoded characters and express them as UTF-8" do
+      rows = ExcelCSV.read write_csv("Ad Id,Fahrräder", encoding: 'Windows-1252')
+      expect(rows).to eq([["Ad Id", "Fahrräder"]])
+    end
+
+    it "should pass options through to CSV.new" do
+      file = write_csv <<-EOF.strip_heredoc
+        Alpha,Beta
+        55,44
+      EOF
+      rows = ExcelCSV.read file, headers: true
+      expect(rows['Alpha'][0]).to eq("55")
+      expect(rows['Beta'][0]).to eq("44")
+    end
+
+    it "should take an optional block" do
+      ExcelCSV.read write_csv("Ad ID;Custom Code\n") do |row|
+        expect(row).to eq(['Ad ID', 'Custom Code'])
+      end
+    end
+
+  end
+
+  describe 'parse' do
+    it "should read the CSV from a string, following the same rules as read" do
+      s = <<-EOF.strip_heredoc.encode('Windows-1252')
+        sep=;
+        Alpha;Beta
+        55;Fahrräder
+      EOF
+
+      rows = ExcelCSV.parse s, headers: true
+
+      expect(rows['Alpha'][0]).to eq("55")
+      expect(rows['Beta'][0]).to eq("Fahrräder".encode('Windows-1252'))
+    end
+  end
+
+  describe 'generate' do
+    it "should bitch if you try to invoke it without a block" do
+      expect { ExcelCSV.generate }.to raise_error ArgumentError
+    end
+
+    it "should generate a regular CSV with DOS line endings, adding a sep=, header" do
+      output = ExcelCSV.generate do |csv|
+        csv << %w{Ad Brand}
+        csv << %w{1 Tre"k}
+      end
+      expect(output).to eq(%Q(sep=,\r\nAd,Brand\r\n1,"Tre""k"\r\n))
+    end
+
+    it "should convert UTF-8 characters in to Windows-1252" do
+      output = ExcelCSV.generate { |c| c << %w{Fahrräder} }
+      expect(output).to eq("sep=,\r\nFahrräder\r\n".encode('Windows-1252'))
+    end
+
+    it "should replace non-encodable characters with a placeholder" do
+      output = ExcelCSV.generate { |c| c << %w{hai壱} }
+      expect(output).to eq("sep=,\r\nhai?\r\n")
+    end
+
+    it "should pass through options to CSV.generate" do
+      output = ExcelCSV.generate(force_quotes: true) { |c| c << %w{a b} }
+      expect(output).to eq(%Q(sep=,\r\n"a","b"\r\n))
+    end
+  end
+
+  describe "write" do
+    it "should write a CSV to a file, following the same rules as generate" do
+      @spreadsheet_file = Tempfile.new(%w{spreadsheet .csv})
+      @spreadsheet_file.close
+      ExcelCSV.write(@spreadsheet_file.path, force_quotes: true) do |csv|
+        csv << %w{Ad hai壱}
+        csv << %w{1 Fahrräder}
+      end
+      contents = File.open(@spreadsheet_file.path, 'rb').read
+      expected = %Q(sep=,\r\n"Ad","hai?"\r\n"1","Fahrräder"\r\n).encode('Windows-1252')
+      expect(contents.bytes).to eq(expected.bytes)
+    end
+  end
+end
