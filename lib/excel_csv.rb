d1648fe73f4b69434e23e224afc7577214accfb0
Michael Pearson <mipearson@gmail.com>
2014-07-17 11:24:51 +1000

    Rubocop

--- lib/excel_csv.rb
+++ lib/excel_csv.rb
@@ -5,7 +5,6 @@ require 'csv'
 # Read & Write Microsoft Excel compliant CSV files, including basic
 # support for Excel running on Windows in Germany.
 class ExcelCSV
-
   class << self
     # Encoding used to read, generate and write CSV.
     #
@@ -19,16 +18,18 @@ class ExcelCSV
     # Accepts the +encoding+ option, otherwise uses +default_encoding+
     #
     # Other +options+ and a block are passed directly to CSV's +parse+ method.
-    def read filename, options = {}, &block
+    def read(filename, options = {}, &block)
       encoding = options.delete(:encoding) || default_encoding
-      file = File.open filename, external_encoding: encoding, internal_encoding: 'UTF-8'
+      file = File.open(
+        filename, external_encoding: encoding, internal_encoding: 'UTF-8'
+      )
       _read file, options, &block
     end
 
     # Parse an Excel-generated CSV string.
     #
     # +options+ and a block are passed directly to CSV's +parse+ method.
-    def parse string, options = {}, &block
+    def parse(string, options = {}, &block)
       file = StringIO.new(string)
       _read file, options, &block
     end
@@ -36,8 +37,8 @@ class ExcelCSV
     # Generate an Excel-compatiable CSV string using +default_encoding+.
     #
     # +options+ and a block are passed directly to CSV's +generate+ method.
-    def generate options = {}, &block
-      raise ArgumentError, "Block required" unless block_given?
+    def generate(options = {}, &block)
+      fail ArgumentError, 'Block required' unless block_given?
       s = CSV.generate({ row_sep: "\r\n" }.merge(options), &block)
       encode_for_excel("sep=,\r\n" + s)
     end
@@ -45,8 +46,8 @@ class ExcelCSV
     # Write an Excel-compatiable CSV to disk using +default_encoding+.
     #
     # +options+ and a block are passed directly to CSV's +generate+ method.
-    def write filename, options = {}, &block
-      raise ArgumentError, "Block required" unless block_given?
+    def write(filename, options = {}, &block)
+      fail ArgumentError, 'Block required' unless block_given?
       File.open(filename, 'wb') do |f|
         f.write generate(options, &block)
       end
@@ -54,19 +55,19 @@ class ExcelCSV
 
     private
 
-    def _read file, options, &block
+    def _read(file, options, &block)
       col_sep = discover_col_sep file
       CSV.parse(file, { col_sep: col_sep }.merge(options), &block)
     end
 
     # Microsoft Excel, on Mac and Windows, expects to import ANSI (Windows-1252)
-    # rather than UTF-8. Importing UTF-8 is possible, but only in Excel 2013, and
-    # has problems with line endings.
-    def encode_for_excel s
+    # rather than UTF-8. Importing UTF-8 is possible, but only in Excel 2013,
+    # and has problems with line endings.
+    def encode_for_excel(s)
       s.encode(default_encoding, invalid: :replace, undef: :replace)
     end
 
-    def discover_col_sep file
+    def discover_col_sep(file)
       first_line = file.readline
       # Search for sep=x first line & return separator
       found_sep = /^sep=(.)$/.match(first_line.strip)
